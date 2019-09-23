6246f5aaa214fe6a8ddbe3bb5c3078c00c8865f3
wakanyan <kyoto.kobe.1126.yuto@gmail.com>
2019-09-23 13:17:14 +0900

    Initial Commit

---
+++ stopwatch/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  stopwatch
+//
+//  Created by Yuto Wakabayashi on 2019/09/16.
+//  Copyright © 2019 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var label : UILabel!
+    
+    var count: Float = 0.0
+    
+    var timer: Timer = Timer()
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func start(){
+        timer = Timer.scheduledTimer(timeInterval: 0.01,
+                                     target: self,
+                                     selector: #selector(self.up),
+                                     userInfo: nil,
+                                     repeats: true
+            )
+    }
+    
+    @IBAction func stop(){
+        if timer.isValid {
+            timer.invalidate()
+        }
+    }
+    
+    
+    @objc func up(){
+        count = count + 0.01
+        label.text = String(format: "%.2f", count)
+    }
+}
+
+
+
